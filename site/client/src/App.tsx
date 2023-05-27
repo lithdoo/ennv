ede080efbe5d04b864b6d024702ad3a89fe8b383
LikeTheDoge <130959182+lithdoo@users.noreply.github.com>
2023-05-27 14:57:45 +0800

    添加 icon 组件

--- site/client/src/App.tsx
+++ site/client/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { EnIcon } from './components/base/icon'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <EnIcon family='i_file' name="excel"></EnIcon>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
